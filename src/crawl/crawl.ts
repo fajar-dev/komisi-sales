5fe673b6954c93868e7f437423ab3ad12aecc80c
fajar-dev <fajarrivaldi2015@gmail.com>
2026-02-05 08:33:37 +0700

    fix: date range crawl

--- src/crawl/crawl.ts
+++ src/crawl/crawl.ts
@@ -17,7 +17,7 @@ class Crawl {
             console.log("Invoice crawl finished.");
             console.log("Starting the employee crawl...");
             await this.employeeCrawl.crawlEmployee();
-            console.log("Employee crawl finished.");
+            // console.log("Employee crawl finished.");
             process.exit(0); 
         } catch (error) {
             console.error("Error running the crawl:", error);
